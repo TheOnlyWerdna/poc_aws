9a51e3885a6f89e5414bf9a8e82622608c5dd5ec
Andrew <andrew@Andrews-Mac-Studio.local>
2023-02-11 10:33:36 -0500

    first commit

---
+++ spti-backend/src/test/java/com/nbc/sptibackend/SptiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbc.sptibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
